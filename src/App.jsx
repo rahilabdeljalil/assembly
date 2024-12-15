8dc44c8c73dfdf35a84f04b9c60bb6747d9be323
rahilabdeljalil <rahilabdo17@gmail.com>
2024-12-15 12:36:51 +0100

    first part finshed

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,76 @@
-import { useState } from 'react'
-import './index.css'
-
+import { useState } from "react";
+import { languages } from "./language";
+import "./index.css";
+import { clsx } from "clsx";
 function App() {
-  const [count, setCount] = useState(0)
+  const [currentWord, setCurrentWord] = useState("react");
+  const [guessedWord, setGuessedWord] = useState([]);
+  const alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+  function handleClick(alph) {
+    setGuessedWord((prevGussedWord) =>
+      !prevGussedWord.includes(alph)
+        ? [...prevGussedWord, alph]
+        : prevGussedWord
+    );
+  }
+
+  const keyboardElements = alphabet.split("").map((alph) => {
+    const isGuessed = guessedWord.includes(alph);
+    const isCorrect = currentWord.includes(alph);
+    const className = clsx("keyboard", {
+      "correct-color": isGuessed && isCorrect,
+      "wrong-color": isGuessed && !isCorrect,
+    });
+    return (
+      <button
+        key={alph}
+        onClick={() => handleClick(alph)}
+        className={className}
+      >
+        {alph.toUpperCase()}
+      </button>
+    );
+  });
+
+  const languageElements = languages.map((lang) => (
+    <span
+      key={lang.name}
+      style={{
+        backgroundColor: lang.backgroundColor,
+        color: lang.color,
+      }}
+      className="chip"
+    >
+      {lang.name}
+    </span>
+  ));
+
+  const letterElements = currentWord.split("").map((letter) => (
+    <span key={letter} className="letter">
+      {letter}
+    </span>
+  ));
 
   return (
     <main>
-    <header>
+      <header>
         <h1>Assembly: Endgame</h1>
-        <p>Guess the word within 8 attempts to keep the
-        programming world safe from Assembly!</p>
-    </header>
-    <section className="game-status">
+        <p>
+          Guess the word within 8 attempts to keep the programming world safe
+          from Assembly!
+        </p>
+      </header>
+      <section className="game-status">
         <h2>You win!</h2>
         <p>Well done! 🎉</p>
-    </section>
-    <section className="language-chips">
-        {languageElements}
-    </section>
-    <section className="word">
-        {letterElements}
-    </section>
-    <section className="keyboard">
-        {keyboardElements}
-    </section>
-    <button className="new-game">New Game</button>
-</main>
-  )
+      </section>
+      <section className="language-chips">{languageElements}</section>
+      <section className="word">{letterElements}</section>
+      <section className="keyboard">{keyboardElements}</section>
+      <button className="new-game">New Game</button>
+    </main>
+  );
 }
 
-export default App
+export default App;
